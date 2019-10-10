b1cfab25fe24c5ca23e0507402107c43149e8a4c
Sean Apsey <ssapsey@mavs.coloradomesa.edu>
2019-10-10 10:36:42 -0600

    finished all kattis problems

--- Kattis/2.6/anotherCandies.py
+++ Kattis/2.6/anotherCandies.py
@@ -1,35 +1,37 @@
 import sys
 
+def answer(candies):
+    n = len(candies)
+    numCandies = 0
+    for i in range(0, len(candies)):
+        numCandies += candies[i]
+        if numCandies > n:
+            numCandies = numCandies % n
+    if numCandies == 0 or numCandies == n:
+        return 'YES'
+    else:
+        return 'NO'
 
 def solve():
     t = int(input())
-    temp = input()
     candies = []
-    total = 0
-    answers = []
     for i in range(0, t):
-        if i == 0:
-            n = int(input())
-        else:
-            temp = input()
-            n = int(input())       
+        temp = input()
+        n = int(input())       
         for j in range(0, n):
             temp = int(input())
             candies.append(temp)
-        for c in candies:
-            total += c
-        if total % n == 0:
-            answers.append('YES')
-        else:
-            answers.append('NO')     
-        candies.clear()
-
-    for a in answers:
-        print(a)
+        print(answer(candies))
+        candies.clear()        
 
 def test():
-    assert(1 == 1)       
-
+    assert(answer([1, 2, 3]) == 'YES')
+    assert(answer([1, 2, 3, 4]) == 'NO')
+    assert(answer([350266329, 404652733, 83317342, 648326556, 667178683, 
+    630563849, 667816293, 189838785, 714805318, 133289603, 151020189, 971511141,
+    887355247, 498802924, 812189941, 414983667, 401196951, 971177252, 119769112, 722967398]) == 'NO')
+    assert(answer([100000000000000000000, 10000000000000000000000000000]) == 'YES')
+    print('All test cases passed!')  
 
 if __name__ == "__main__":
     if len(sys.argv) > 1 and sys.argv[1] == 'test':
